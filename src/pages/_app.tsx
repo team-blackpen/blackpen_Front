92811d9da8095f3e26c736ed98e1b2d228103218
devyouth94 <devyouth94@gmail.com>
2023-05-31 14:59:19 +0900

    Feat: _app.tsx 초기 설정 및 초기 페이지 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Global, ThemeProvider } from "@emotion/react";
+
+import globalStyles from "shared/styles/globalStyles";
+import { theme } from "shared/styles/globalTheme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Global styles={globalStyles} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }

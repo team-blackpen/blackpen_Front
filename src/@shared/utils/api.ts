fe28b6a37cfd71e55fdf3a282dbb4a94c380220c
devyouth94 <devyouth94@gmail.com>
2023-07-14 23:45:55 +0900

    Feat: 로그인시 정보 쿠키 설정 및 interceptors 설정

--- src/@shared/utils/api.ts
+++ src/@shared/utils/api.ts
@@ -1,13 +1,36 @@
 import axios from "axios";
+import { getCookie, setCookie } from "cookies-next";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
+api.interceptors.request.use((config) => {
+  const token = getCookie("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
 
-export const kakaoLogin = async (code: string) => {
-  const { data } = await api.get("/auth/kakao/callback", { params: { code } });
+interface GetLoginData {
+  data: {
+    accessToken: string;
+    nickname: string;
+    user_no: number;
+  };
+}
 
-  console.log(data);
+export const kakaoLogin = async (code: string) => {
+  const { data } = await api.get<GetLoginData>("/auth/kakao/callback", {
+    params: { code },
+  });
+
+  setCookie("token", data.data.accessToken);
+  setCookie("nickname", data.data.nickname);
+  setCookie("userNo", data.data.user_no);
 };
